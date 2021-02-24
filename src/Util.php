6b67fa6c3b3dcf0337e760467b4f65a3a4e9132b
陈振业 <chenzy@yiche99.com>
2021-02-24 23:20:10 +0800

    first

---
+++ src/Util.php
@@ -0,0 +1,11 @@
+<?php
+class Util{
+    public static function test(){
+        echo 'this is test';
+    }
+    
+    public static function test2(){
+        echo 'this is test2';
+    }
+    
+}
